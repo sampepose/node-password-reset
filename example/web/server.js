ad33c364e746b099e6265eb589717edc74e1c1b0
Terry Huang <terryh.tp@gmail.com>
2013-08-16 10:12:58 +0800

    update example and README

--- example/web/server.js
+++ example/web/server.js
@@ -7,38 +7,57 @@ var app = express.createServer();
 app.use(express.static(__dirname));
 app.use(require('sesame')()); // for sessions
 
+// example nodemailer config here
 var forgot = require('../../')({
-    uri : 'http://localhost:8080/password_reset',
-    from : 'password-robot@localhost',
-    host : 'localhost', port : 25,
+  uri: 'http://localhost:8080/password_reset',
+  from: 'password-robot@localhost',
+  transportType: 'SMTP',
+  transportOptions: {
+    service: "Gmail",
+    auth: {
+      user: "youmailaccount@gmail.com",
+      pass: "password"
+    }
+  }
 });
+
+
 app.use(forgot.middleware);
 
-app.post('/forgot', express.bodyParser(), function (req, res) {
-    var email = req.body.email;
-    var reset = forgot(email, function (err) {
-        if (err) res.end('Error sending message: ' + err)
-        else res.end('Check your inbox for a password reset message.')
-    });
-    
-    reset.on('request', function (req_, res_) {
-        req_.session.reset = { email : email, id : reset.id };
-        fs.createReadStream(__dirname + '/forgot.html').pipe(res_);
-    });
+app.post('/forgot', express.bodyParser(), function(req, res) {
+  var email = req.body.email;
+
+  var callback = {
+    error: function(err) {
+      res.end('Error sending message: ' + err);
+    },
+    success: function(success) {
+      res.end('Check your inbox for a password reset message.');
+    }
+  };
+  var reset = forgot(email, callback);
+
+  reset.on('request', function(req_, res_) {
+    req_.session.reset = {
+      email: email,
+      id: reset.id
+    };
+    fs.createReadStream(__dirname + '/forgot.html').pipe(res_);
+  });
 });
 
-app.post('/reset', express.bodyParser(), function (req, res) {
-    if (!req.session.reset) return res.end('reset token not set');
-    
-    var password = req.body.password;
-    var confirm = req.body.confirm;
-    if (password !== confirm) return res.end('passwords do not match');
-    
-    // update the user db here
-    
-    forgot.expire(req.session.reset.id);
-    delete req.session.reset;
-    res.end('password reset');
+app.post('/reset', express.bodyParser(), function(req, res) {
+  if (!req.session.reset) return res.end('reset token not set');
+
+  var password = req.body.password;
+  var confirm = req.body.confirm;
+  if (password !== confirm) return res.end('passwords do not match');
+
+  // update the user db here
+
+  forgot.expire(req.session.reset.id);
+  delete req.session.reset;
+  res.end('password reset');
 });
 
 app.listen(8080);
